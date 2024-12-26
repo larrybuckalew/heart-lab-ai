f5d98cea6cc06bf521873fc894936c0e8acfbbaf
Larry Buckalew <larry.buckalew@gmail.com>
2024-12-26 18:24:46 -0500

    Add email templates and service files

---
+++ server/email-templates.js
@@ -0,0 +1,1 @@
+const emailTemplates = { /* Full email templates here */ };
--- server/server.js
+++ server/server.js
@@ -1,5 +1,1 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-require('dotenv').config();
-// Rest of server code
+const express = require('express'); /* Full server code here */;
