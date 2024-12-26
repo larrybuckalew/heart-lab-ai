36713b5d709bc825c79ce7747f745dfa1ce16ccd
Larry Buckalew <larry.buckalew@gmail.com>
2024-12-26 18:34:18 -0500

    Add enterprise and industry-specific templates

---
+++ server/templates/nurture/industry-specific.js
@@ -0,0 +1,17 @@
+const industryTemplates = {
+    healthcare: {
+        introduction: (data) => `...`,
+        compliance: (data) => `...`,
+        caseStudies: (data) => `...`
+    },
+    finance: {
+        introduction: (data) => `...`,
+        security: (data) => `...`,
+        roi: (data) => `...`
+    },
+    retail: {
+        introduction: (data) => `...`,
+        customerService: (data) => `...`,
+        automation: (data) => `...`
+    }
+}
