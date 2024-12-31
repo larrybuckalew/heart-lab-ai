7b463e9ce597b5cdc774e3a49af72a1d46bcc7bc
Larry Buckalew <larry.buckalew@gmail.com>
2024-12-30 22:19:47 -0500

    fix: update next.config.js for proper standalone output

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined
+  },
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone'
 }
 
 module.exports = nextConfig
