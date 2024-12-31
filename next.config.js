3d19a37ce03757cf5c9779687601454e72a754a1
Larry Buckalew <larry.buckalew@gmail.com>
2024-12-30 23:25:16 -0500

    fix: simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: undefined
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  }
+  reactStrictMode: true
 }
