f32f5e6c75e6cb38a672752f2a18361564dc8e13
Larry Buckalew <larry.buckalew@gmail.com>
2024-12-30 23:19:26 -0500

    fix: update next.config.js for standalone build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
   output: 'standalone',
-  distDir: '.next',
-  poweredByHeader: false,
-  generateBuildId: async () => {
-    return 'build-' + Date.now()
+  experimental: {
+    outputFileTracingRoot: undefined
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
   }
 }
