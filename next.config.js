ee540f32434ee5733403067c7cb267393800af1a
Larry Buckalew <larry.buckalew@gmail.com>
2024-12-30 23:12:14 -0500

    fix: update next.config.js with build configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: 'standalone',
+  distDir: '.next',
   poweredByHeader: false,
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  }
 }
