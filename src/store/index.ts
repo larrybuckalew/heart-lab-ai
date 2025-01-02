be075c374cb76f71a4025af1d2f059756b52d55c
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-02 10:38:56 -0500

    Enhance state management with advanced Redux slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import researchReducer from './researchSlice';
+import riskPredictionReducer from './riskPredictionSlice';
 
 export const store = configureStore({
   reducer: {
     research: researchReducer,
+    riskPrediction: riskPredictionReducer
   },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: false // Disable serializable check for complex objects
+    })
 });
 
 export type RootState = ReturnType<typeof store.getState>;
