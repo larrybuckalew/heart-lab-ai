31c0f3e0f10cfff1be892a8c2ceecd02fee123b7
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-11 13:27:52 -0500

    Simplify build configuration and dependencies

--- next.config.js
+++ next.config.js
@@ -2,19 +2,7 @@
 module.exports = {
   output: 'standalone',
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      }
-    }
-    return config;
+    unoptimized: true
   }
 };
