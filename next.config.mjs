400577e6b6313f5bda6e5334a067d301f9d72df7
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-11 11:13:32 -0500

    Optimize build configuration for Netlify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
