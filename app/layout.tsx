cf4afda4c8e9f10ebb65cd0148e4aa296ede029c
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-12 20:51:10 -0500

    Restructure for app directory only

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,9 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navigation from '@/components/Navigation'
-import Footer from '@/components/Footer'
-import Script from 'next/script'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Heart Lab AI',
-  description: 'AI Solutions for Small Businesses',
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-J3R5PM89DD"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-J3R5PM89DD');
-          `}
-        </Script>
-      </head>
-      <body className={inter.className}>
-        <Navigation />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
