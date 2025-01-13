9fda6ee59ccae9594bab997658970038d59f01a2
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-12 21:29:23 -0500

    Move CSS dependencies to main dependencies and update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
